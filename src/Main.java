faa0bff28f83542f5c37c22ad1b589a8a57f5b36
PaviMoreira <yahiko_pavi@live.com>
2024-03-14 15:53:30 -0300

    Inserting limitations on input.

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
 
-        System.out.println("Insert a binary number up to 10 digits.");
-        String input = read.nextLine();
-
         int result = 0;
+        String input;
 
-        for(int i = 0; i < input.length(); i++){
-            if(input.charAt(i) == '1'){
+        do {
+            System.out.println("Insert a binary number up to 10 digits.");
+            input = read.nextLine();
+        } while (!isBinaryNumber(input));
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '1') {
                 result += (int) Math.pow(2, (input.length() - 1) - i);
             }
         }
 
         System.out.println("Your number is: " + result);
+    }
+
+    public static boolean isBinaryNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            int x = input.charAt(i);
+            if (x != '0' && x != '1') {
+                System.out.println("Input is not a binary number.");
+                return false;
+            }
+        }
+        return true;
     }
 }
